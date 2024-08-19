38935d568c610ba5dccbc26ca6ecf23aa54036f6
Russian Poker team <russian-poker@alicorn.lv>
2024-08-19 18:05:18 +0500

    error transaction

--- connections/Play.js
+++ connections/Play.js
@@ -63,7 +63,7 @@ class Play extends GameService {
                 } else {
                     const playerData = {
                         ...player,
-                        status: "CHOICE",
+                        status: CHOICE,
                         gameInfo: null,
                         ante: 0,
                         bonus: 0,
@@ -203,11 +203,11 @@ class Play extends GameService {
 
                     } 
                     else {
-                        this.reconnection(socket.id, 'transaction')
+                        this.reconnection(playerId, 'transaction')
                     }
 
                 } else {
-                    this.reconnection(socket.id, 'error')
+                    this.reconnection(playerId, 'error')
                 }
             })
 
@@ -571,11 +571,16 @@ class Play extends GameService {
             bonusResult: player.bonusResult,
             result: player.result,
 
-            transactions: player.transactions
+            transactions: player.transactions,
+
+            transactionError: false,
         }
 
         if (reason === 'error') {
             data.error = true
+        }
+        if (reason === 'transaction') {
+            data.transactionError = true
         }
 
         this.socket.in(this.players[id].socketId).emit('reconnection', data)
@@ -868,7 +873,7 @@ class Play extends GameService {
             const playerData = this.players[id]
 
             /* Send request to ALICORN SERVICE */
-            const balance = process.env.NODE_ENV && process.env.NODE_ENV === "development" ? 2370000 : 2370000 //await this.getBalance(player)
+            const balance = process.env.NODE_ENV && process.env.NODE_ENV === "development" ? 2370000 : await this.getBalance(player)
 
             /* Send the BALANCE to socket client */
             if (balance) {
